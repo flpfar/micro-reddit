64abda46d08e139c5592c6ce953c34d7aa804f6b
sergiomauz <sergio.mauz88@gmail.com>
2020-04-30 16:23:25 -0500

    Added post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :email, :username, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :username, length: {in: 4..15}
+  validates :password, presence: true, length: {minimum: 6 }
+
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_194742) do
+ActiveRecord::Schema.define(version: 2020_04_30_205623) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.string "image_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
